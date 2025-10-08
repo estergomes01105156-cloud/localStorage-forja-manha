3f89a24b5fcece9c540ee8ed53ec3600571342f0
ester <ester.gomes01105156@sesisenaipr.org.br>
2025-10-08 11:45:02 -0300

    Add files via upload

---
+++ Login/script.js
@@ -0,0 +1,43 @@
+// funçaõ de login
+function login(){
+    // javascript puro para captar valores dos inputs
+    //var nome = document.getElementById("nome").value
+    //var senha = document.getElementById("senha").value
+    
+    // uso do jquery para captar valores
+    var nome = $("#nome").val()
+    var senha = $("#senha").val()
+
+    if(nome && senha && nome === "March" && senha === "170310"){
+        const user = {
+            name: nome,
+            dataEntrada: new Date(),
+            id: Math.floor(Math.random() * 100000)
+        }
+        localStorage.setItem("usuario", JSON.stringify(user))
+        window.location.href = "../Loja"
+    }else{
+        document.getElementById("error-moda1").style.display = "flex"
+        document.getElementById("nome").style.borderBottom = "3px solid red"
+        document.getElementById("senha").style.borderBottom = "3px solid red"
+    }
+}
+
+function showPassword(){
+    var inputSenha = document.querySelector('#senha')
+    var img_eye = document.querySelector('#eye')
+
+    if(inputSenha.getAttribute("type") === "password"){
+        inputSenha.setAttribute("type", "text")
+        img_eye.setAttribute("src", "./Images/hide.png")
+    }else{
+        inputSenha.setAttribute("type", "password")
+        img_eye.setAttribute("src", "./Images/view.png")
+    }
+}
+
+function fecharError(){
+    document.getElementById("error-moda1").style.display = "flex"
+    document.getElementById("nome").style.borderBottom = "3px solid red"
+    document.getElementById("senha").style.borderBottom = "3px solid red"
+}
